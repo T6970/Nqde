7ba34055936b1b4f5c920e2318c751dc09f44eea
T6970 <tenaldkyf315@gmail.com>
2025-11-27 15:58:09 +0800

    Initial commit for script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+const editor = document.getElementById('editor')
+import "./mainApi.js"
 
+const extensions = []
+const main = async () => {
+  try {
+    const load = await fetch('../extlist.txt')
+    const extensionNames = (await load.text()).split('\n').filter(name => name.trim() !== '')
+    
+    for (let i = 0; i < extensionNames.length; i++) {
+      const extension = new Worker(`../extensions/${extensionNames[i]}.js`)
+
+      extensions.push(extension)
+    }
+
+    editor.addEventListener('keydown', (e) => {
+      extensions.forEach(extension => {
+        extension.postMessage({type: 'keypress', key: e.key})
+      })
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+main()
