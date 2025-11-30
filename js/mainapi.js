f1d5db8122ce2665e19a853d4d8a5cf987aae97a
T6970 <tenaldkyf315@gmail.com>
2025-11-30 11:40:43 +0800

    Add console messages

--- js/mainapi.js
+++ js/mainapi.js
@@ -5,32 +5,37 @@ export const NqdeExtension = (ext) => {
     const msg = e.data;
     switch (msg.type) {
       case 'newCard':
+        console.log(`Creating ${msg.content.id}`)
         card = document.createElement('div')
-        card.className = `card`;
-        card.style.position = 'absolute';
-        card.style.width = '800px';
-        card.style.height = '600px';
-        card.style.left = '100px';
-        card.style.top  = '100px';
-        card.id = msg.content.id;
-        document.body.appendChild(card);
-        break;
+        card.className = `card`
+        card.style.position = 'absolute'
+        card.style.width = '800px'
+        card.style.height = '600px'
+        card.style.left = '100px'
+        card.style.top  = '100px'
+        card.id = msg.content.id
+        document.body.appendChild(card)
+        break
       case 'addContent':
-        document.getElementById(msg.content.id).innerHTML += msg.content.content;
-        break;
+        console.log(`Adding ${msg.content.content} to ${msg.content.id}`)
+        document.getElementById(msg.content.id).innerHTML += msg.content.content
+        break
       case 'delCard':
-        document.getElementById(msg.content.id).remove();
-        break;
+        console.log(`Deleting ${msg.content.id}`)
+        document.getElementById(msg.content.id).remove()
+        break
       case 'moveCard':
-        const moveCard = document.getElementById(msg.content.id);
-        moveCard.style.left = msg.content.x + 'px';
-        moveCard.style.top  = msg.content.y + 'px';
-        break;
+        console.log(`Moving ${msg.content.id} to (${msg.content.x}, ${msg.content.y})`)
+        const moveCard = document.getElementById(msg.content.id)
+        moveCard.style.left = msg.content.x + 'px'
+        moveCard.style.top  = msg.content.y + 'px'
+        break
       case 'resizeCard':
-        const resizeCard = document.getElementById(msg.content.id);
-        resizeCard.style.width  = msg.content.width + 'px';
-        resizeCard.style.height = msg.content.height + 'px';
-        break;
+        console.log(`Resizing ${msg.content.id} to (${msg.content.width}, ${msg.content.height})`)
+        const resizeCard = document.getElementById(msg.content.id)
+        resizeCard.style.width  = msg.content.width + 'px'
+        resizeCard.style.height = msg.content.height + 'px'
+        break
     }
   }
 }
