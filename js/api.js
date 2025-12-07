e48d8b31f0deda4c585bab1fc24d00b89e892425
T6970 <tenaldkyf315@gmail.com>
2025-12-07 16:59:50 +0800

    Document changes

--- js/api.js
+++ js/api.js
@@ -1,4 +1,14 @@
+/**
+ * API for Nqde extensions
+ */
 export const Nqde = {
+
+  /**
+   * Creates a new card element; it will be positioned at (100, 100) with size 800x600.
+   * @param {string} id ID for the card.
+   * @example Nqde.newCard("myCard")
+   */
+
   newCard (id) {
     if (!id) {
       throw new ReferenceError("No ID provided")
@@ -13,6 +23,12 @@ export const Nqde = {
     card.style.height = "600px"
     document.body.appendChild(card)
   },
+
+  /**
+   * Appends content into a card; existing content will not be removed.
+   * @param {string} id ID of the card.
+   * @param {HTMLElement} content Content to add into the card.
+   */
   addContent (id, content) {
     if (!id) {
       throw new ReferenceError("No ID provided")
@@ -26,6 +42,12 @@ export const Nqde = {
     }
     card.innerHTML += content
   },
+
+  /**
+   * Replaces the content of a card. Doesn't keep existing content.
+   * @param {string} id ID of the card.
+   * @param {HTMLElement} content New content of the card.
+   */
   setContent (id, content) {
     if (!id) {
       throw new ReferenceError("No ID provided")
@@ -39,6 +61,11 @@ export const Nqde = {
     }
     card.innerHTML = content
   },
+
+  /**
+   * Deletes a card.
+   * @param {string} id ID of the card.
+   */
   deleteCard (id) {
     if (!id) {
       throw new ReferenceError("No ID provided")
@@ -49,6 +76,14 @@ export const Nqde = {
     }
     card.remove()
   },
+
+  /**
+   * Moves a card to a new position. Positions are in pixels.
+   * @param {string} id ID of the card.
+   * @param {number} x New x position in pixels.
+   * @param {number} y New y position in pixels.
+   */
+
   moveCard (id, x, y) {
     if (!id) {
       throw new ReferenceError("No ID provided")
@@ -60,6 +95,13 @@ export const Nqde = {
     card.style.left = x + "px"
     card.style.top = y + "px"
   },
+
+  /**
+   * Resizes a card. Sizes are in pixels.
+   * @param {string} id ID of the card.
+   * @param {number} width New width in pixels.
+   * @param {number} height New height in pixels.
+   */
   resizeCard (id, width, height) {
     if (!id) {
       throw new ReferenceError("No ID provided")
