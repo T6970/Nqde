ea2385adae2ae54da3ef02292adb12eee6fa979a
T6970 <tenaldkyf315@gmail.com>
2025-12-08 13:25:31 +0800

    Add newElement()
    Internal and for demo purposes only

--- src/demo.js
+++ src/demo.js
@@ -1,13 +1,19 @@
 import { Nqde } from "./api.js";
 
 Nqde.newCard("demo")
-Nqde.addContent("demo", '<h1>Nqde</h1>')
+Nqde.addContent("demo", newElement("h1", "Nqde"))
 Nqde.resizeCard("demo", 400, 300)
 
 const innerCard = document.createElement('div')
 innerCard.style.height = "175px"
 innerCard.classList = "nest card"
-innerCard.innerHTML = "<p>This is an inner card.</p>"
+innerCard.innerHTML = newElement("p", "This is an inner card.")
 
 Nqde.addContent("demo", innerCard.outerHTML)
 Nqde.makeDraggable("demo")
+
+const newElement = (type, content) => {
+  const el = document.createElement(type)
+  el.innerHTML = content
+  return el.outerHTML
+}
